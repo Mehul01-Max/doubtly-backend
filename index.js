06484790767d62e1cf2880b77b8319a5bb614c4c
Mehul Agarwal <agarwalmehul423@gmail.com>
2025-02-22 22:25:04 +0530

    enabling cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {solution} = require("./routes/solution");
 const app = express();
 dotenv.config();
 app.use(express.json())
+app.use(cors());
 app.get('/',(req,res)=>{
     res.json({
         message : "hi"
