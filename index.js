9d32fc963e94ac2b73ded776ab414cd7e490911c
Mehul Agarwal <agarwalmehul423@gmail.com>
2025-03-13 23:52:07 +0530

    minor change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 dotenv.config();
 app.use(express.json());
-app.use(cors({}));
+app.use(cors());
 app.use(cookieParser());
 app.get("/", (req, res) => {
   res.json({
